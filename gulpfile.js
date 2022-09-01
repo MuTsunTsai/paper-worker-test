60040054d6b7cf8b752e8552b938fc2c230c150a
MuTsunTsai <don.m.t.tsai@gmail.com>
2022-09-01 16:38:52 +0800

    Switch to pnpm, update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,22 @@
 let gulp = require('gulp');
 let ts = require('gulp-typescript');
-let ifAnyNewer = require('gulp-if-any-newer');
+let newer = require('gulp-newer');
 
 let projMain = ts.createProject('src/main/tsconfig.json');
 let projWorker = ts.createProject('src/worker/tsconfig.json');
 
 gulp.task('buildMain', () =>
 	projMain.src()
-		.pipe(ifAnyNewer("dist", { filter: 'main.js' }))
+		.pipe(newer("dist/main.js"))
 		.pipe(projMain())
-		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest('docs'))
 );
 
 gulp.task('buildWorker', () =>
 	projWorker.src()
-		.pipe(ifAnyNewer("dist", { filter: 'worker.js' }))
+		.pipe(newer("dist/worker.js"))
 		.pipe(projWorker())
-		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest('docs'))
 );
 
-gulp.task('pause', cb => setTimeout(cb, 1000));
-
-gulp.task('default', gulp.series('buildMain', 'buildWorker', 'pause'));
+gulp.task('default', gulp.series('buildMain', 'buildWorker'));
